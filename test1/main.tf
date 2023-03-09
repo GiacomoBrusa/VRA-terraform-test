9a8bf4f7ff703fde6879d262d2dfb2e84a8cd851
Giacomo Brusamolin <38219089+GiacomoBrusa@users.noreply.github.com>
2023-03-09 18:52:57 +0100

    Update main.tf

--- test1/main.tf
+++ test1/main.tf
@@ -1,19 +1,23 @@
-inputs:
-  instance_type:
-    type: string
-    default: t2.micro
-    description: AWS instance type
-  department:
-    type: string
-    description: Department tag
-resources:
-  terraform:
-    type: Cloud.Terraform.Configuration
-    properties:
-      variables:
-        instance_type: '${input.instance_type}'
-        department: '${input.department}'
-      providers:
-        - name: aws
-        # List of available cloud zones: blueprint-aws/us-east-1
-        cloudZone: blueprint-aws/eu-south-1
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
